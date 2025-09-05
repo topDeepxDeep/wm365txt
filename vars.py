472b76339b700a243cab852a9ce7db0d2035bf49
topDeepxDeep <top.noch19710@gmail.com>
2025-09-06 00:40:32 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,14 +3,14 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "3498045"))
-API_HASH = environ.get("API_HASH", "229441a8b355de58ba025be657c82ea3")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8373086447:AAEmSLV-Q85ZtvrX2VCeiTHuYWDCmTOeP7E")
+API_ID = int(environ.get("API_ID", "27473563"))
+API_HASH = environ.get("API_HASH", "bc2ea0765ac96bb474891b0243f44390")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7086145092:AAELtQW8GMdkA5UOHknyrY89zOXDv8TjZTU")
 
-OWNER = int(environ.get("OWNER", "6478217960"))
+OWNER = int(environ.get("OWNER", "6363345131"))
 CREDIT = environ.get("CREDIT", "ᴹᴿ🌿⃝🅺ιʟͥʟͣᴇᷟʀ✍࿐ཽ༵")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '6478217960').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '6363345131,6478217960').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
 AUTH_USER = os.environ.get('AUTH_USERS', '6478217960').split(',')
@@ -20,4 +20,5 @@ if int(OWNER) not in AUTH_USERS:
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
